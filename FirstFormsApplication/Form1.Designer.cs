eea828eee453ff6014fe5a25d2ac6e23e35c4e3f
stevenmartinez123 <stevenmartinez1494@gmail.com>
2021-08-22 12:25:18 -0700

    Calculator working. Updating exception handling left

--- FirstFormsApplication/Form1.Designer.cs
+++ FirstFormsApplication/Form1.Designer.cs
@@ -40,6 +40,8 @@ namespace FirstFormsApplication
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.ResultLabel = new System.Windows.Forms.Label();
             this.AnswerLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.OperationTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // FirstNumber
@@ -52,11 +54,12 @@ namespace FirstFormsApplication
             this.FirstNumber.Size = new System.Drawing.Size(133, 23);
             this.FirstNumber.TabIndex = 0;
             this.FirstNumber.Text = "First Number: ";
+            this.FirstNumber.Click += new System.EventHandler(this.FirstNumber_Click);
             // 
             // firstNumberPrompt
             // 
             this.firstNumberPrompt.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.firstNumberPrompt.Location = new System.Drawing.Point(369, 106);
+            this.firstNumberPrompt.Location = new System.Drawing.Point(369, 105);
             this.firstNumberPrompt.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.firstNumberPrompt.Name = "firstNumberPrompt";
             this.firstNumberPrompt.Size = new System.Drawing.Size(209, 30);
@@ -73,53 +76,54 @@ namespace FirstFormsApplication
             this.secondNumber.Size = new System.Drawing.Size(154, 23);
             this.secondNumber.TabIndex = 2;
             this.secondNumber.Text = "Second Number: ";
+            this.secondNumber.Click += new System.EventHandler(this.secondNumber_Click);
             // 
             // secondNumberPrompt
             // 
             this.secondNumberPrompt.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.secondNumberPrompt.Location = new System.Drawing.Point(369, 205);
+            this.secondNumberPrompt.Location = new System.Drawing.Point(369, 198);
             this.secondNumberPrompt.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.secondNumberPrompt.Name = "secondNumberPrompt";
             this.secondNumberPrompt.Size = new System.Drawing.Size(209, 30);
             this.secondNumberPrompt.TabIndex = 3;
+            this.secondNumberPrompt.TextChanged += new System.EventHandler(this.secondNumberPrompt_TextChanged);
             // 
             // selectOperation
             // 
             this.selectOperation.AutoSize = true;
             this.selectOperation.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.selectOperation.Location = new System.Drawing.Point(87, 300);
+            this.selectOperation.Location = new System.Drawing.Point(87, 268);
             this.selectOperation.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.selectOperation.Name = "selectOperation";
-            this.selectOperation.Size = new System.Drawing.Size(170, 23);
+            this.selectOperation.Size = new System.Drawing.Size(171, 92);
             this.selectOperation.TabIndex = 4;
-            this.selectOperation.Text = "Select Operation : ";
+            this.selectOperation.Text = "Select Operation:\r\n+ for Addition\r\n- for Subtraction\r\n* for Multiplication\r\n";
+            this.selectOperation.Click += new System.EventHandler(this.selectOperation_Click);
             // 
             // operationDropDown
             // 
-            this.operationDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.operationDropDown.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.operationDropDown.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.operationDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.operationDropDown.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.operationDropDown.FormattingEnabled = true;
-            this.operationDropDown.Items.AddRange(new object[] {
-            "Addition",
-            "Subtraction",
-            "Multiplication"});
-            this.operationDropDown.Location = new System.Drawing.Point(369, 291);
+            this.operationDropDown.Location = new System.Drawing.Point(-6, 544);
             this.operationDropDown.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.operationDropDown.Name = "operationDropDown";
-            this.operationDropDown.Size = new System.Drawing.Size(209, 31);
+            this.operationDropDown.Size = new System.Drawing.Size(10, 10);
             this.operationDropDown.TabIndex = 5;
             // 
             // calculationButton
             // 
             this.calculationButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.calculationButton.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.calculationButton.Location = new System.Drawing.Point(369, 379);
+            this.calculationButton.Location = new System.Drawing.Point(371, 368);
             this.calculationButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.calculationButton.Name = "calculationButton";
             this.calculationButton.Size = new System.Drawing.Size(211, 52);
             this.calculationButton.TabIndex = 6;
             this.calculationButton.Text = "Calculate:";
             this.calculationButton.UseVisualStyleBackColor = true;
+            this.calculationButton.Click += new System.EventHandler(this.calculationButton_Click);
             // 
             // imageList1
             // 
@@ -130,11 +134,12 @@ namespace FirstFormsApplication
             // ResultLabel
             // 
             this.ResultLabel.AutoSize = true;
-            this.ResultLabel.Location = new System.Drawing.Point(87, 456);
+            this.ResultLabel.Location = new System.Drawing.Point(51, 469);
             this.ResultLabel.Name = "ResultLabel";
             this.ResultLabel.Size = new System.Drawing.Size(71, 23);
             this.ResultLabel.TabIndex = 7;
             this.ResultLabel.Text = "Result:";
+            this.ResultLabel.Click += new System.EventHandler(this.ResultLabel_Click);
             // 
             // AnswerLabel
             // 
@@ -144,11 +149,32 @@ namespace FirstFormsApplication
             this.AnswerLabel.Size = new System.Drawing.Size(0, 23);
             this.AnswerLabel.TabIndex = 8;
             // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.SystemColors.Window;
+            this.outputLabel.Location = new System.Drawing.Point(128, 458);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(311, 34);
+            this.outputLabel.TabIndex = 9;
+            this.outputLabel.Click += new System.EventHandler(this.outputLabel_Click);
+            // 
+            // OperationTextBox
+            // 
+            this.OperationTextBox.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OperationTextBox.Location = new System.Drawing.Point(371, 284);
+            this.OperationTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.OperationTextBox.Name = "OperationTextBox";
+            this.OperationTextBox.Size = new System.Drawing.Size(209, 30);
+            this.OperationTextBox.TabIndex = 10;
+            this.OperationTextBox.TextChanged += new System.EventHandler(this.OperationTextBox_TextChanged);
+            // 
             // Calculate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(728, 560);
+            this.ClientSize = new System.Drawing.Size(682, 566);
+            this.Controls.Add(this.OperationTextBox);
+            this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.AnswerLabel);
             this.Controls.Add(this.ResultLabel);
             this.Controls.Add(this.calculationButton);
@@ -180,6 +206,8 @@ namespace FirstFormsApplication
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.Label ResultLabel;
         private System.Windows.Forms.Label AnswerLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.TextBox OperationTextBox;
     }
 }
 
--- FirstFormsApplication/Form1.cs
+++ FirstFormsApplication/Form1.cs
@@ -12,6 +12,13 @@ namespace FirstFormsApplication
 {
     public partial class Calculate : Form
     {
+
+        //declare variables for user input and result 
+        double a = 0; 
+        double b = 0;  
+        string option = "";
+        double result = 0.0; 
+
         public Calculate()
         {
             InitializeComponent();
@@ -19,12 +26,72 @@ namespace FirstFormsApplication
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            int num1, num2, result;
-          //  num1 = Convert.ToInt32(txtFirst.txt); 
+      
 
         }
 
         private void firstNumberPrompt_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void FirstNumber_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void secondNumber_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void secondNumberPrompt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectOperation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculationButton_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(firstNumberPrompt.Text);
+            b = Convert.ToDouble(secondNumberPrompt.Text);
+            option = Convert.ToString(OperationTextBox.Text);
+
+            if (option.Equals("+"))
+            { 
+                result = a + b;
+                outputLabel.Text = result.ToString();
+            } else if (option.Equals("-"))
+            {
+                result = a - b;
+                outputLabel.Text = result.ToString();
+
+            } else if (option.Equals("*"))
+
+            {
+                result = a * b;
+                outputLabel.Text = result.ToString();
+            } else
+            {
+                outputLabel.Text = "Error: Please check operation symbol."; 
+            }
+        }
+
+        private void ResultLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outputLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OperationTextBox_TextChanged(object sender, EventArgs e)
         {
 
         }
